fd77173aba706a25c210e14a88bf3f3cc8b0937a
van-dai-vuong <vuongdai@gmail.com>
2024-04-15 15:41:55 -0400

    update intro

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Coupling LSTM Neural Networks and State-Space Models through Analytically Tractable Inference
 This repos contains the matlab codes to reproduce the results for the paper "Coupling LSTM Neural Networks and State-Space Models through Analytically Tractable Inference" published in the International Journal of Forecasting.
+
 (1) To load the saved predictions and calculate the test metrics:
 run scripts in the "metrics" folder, e.g. "metrics_electricity.m"
+
 (2) To run the code and obtain the predictions for each dataset:
 run scripts in the "config" folder , e.g. "electricity_2014_03_31.m"
+
 (3) To run examples using TAGI-LSTM and the TAGI-LSTM/SSM hybrid model:
 runs scripts in the "examples" folder
 - The "synthetic_LSTM_smoothing.m" file is to perform smoothing in TAGI-LSTM. In this example, smoothing is used to infer the past observations before the training time.
--- functions/ensem.m
+++ functions/ensem.m
@@ -8,19 +8,7 @@
 % Copyright (c) 2020 Luong-Ha nguyen  & James-A. Goulet  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 classdef ensem
-    methods (Static)        
-        function [myEnsem, SyEnsem, w] = ensembleLL(y, Sy, LL)
-            likelihood = exp(LL);
-            w  = likelihood./sum(likelihood,2);
-            myEnsem  = sum(y.*w,2);
-            SyEnsem = sum(Sy.*w + w.*(y-myEnsem).^2,2);
-        end
-        
-        function [myEnsem, SyEnsem] = ensembleWeight(y, Sy, w)
-            myEnsem  = sum(y.*w,2);
-            SyEnsem = sum(Sy.*w + w.*(y-myEnsem).^2,2);
-        end
-        
+    methods (Static)                
         function [myEnsem, SyEnsem] = ensembleWeight_cell(y, Sy, w)
             n = size(y,1);
             nbTs = size(y{1},2);
@@ -35,37 +23,6 @@ classdef ensem
             SyEnsem  = sum(Sytemp.*w + w.*(ytemp-myEnsem).^2,2);
             myEnsem  = reshape(myEnsem,nbPoint,nbTs);
             SyEnsem  = reshape(SyEnsem,nbPoint,nbTs);
-        end
-
-        function [myEnsem] = ensembleWeightBackProp_cell(y, w)
-            n = size(y,1);
-            nbTs = size(y{1},2);
-            nbPoint = size(y{1},1);
-            ytemp  = [];
-            for i = 1:n
-                ytemp  = cat(2, ytemp, reshape(y{i}, nbPoint, 1, []));
-            end
-            myEnsem  = sum(ytemp.*w,2);
-            myEnsem  = reshape(myEnsem,nbPoint,nbTs);
-        end
-        
-        function [myEnsem, SyEnsem, w] = ensemble_dirichlet(y, Sy, LL, alpha)            
-            ns = 1E6; % number of Monte-Carlo samples
-            nb = numel(LL);
-            r  = ensem.drchrnd(alpha*ones(1,nb),ns);
-            likelihood = exp(LL);
-            % Posterior f(p|D)=f(D|m)*dirichlet(p;alpha)
-            w  = likelihood.*r./sum(likelihood.*r,2);
-            w  = mean(w);
-            myEnsem  = sum(y.*w,2);
-            SyEnsem = sum(Sy.*w + w.*(y-myEnsem).^2,2);
-        end
-       
-        function r = drchrnd(a,n)
-            % take a sample from a dirichlet distribution
-            p = length(a);
-            r = gamrnd(repmat(a,n,1),1,n,p);
-            r = r ./ repmat(sum(r,2),1,p);
         end
     end
 end
